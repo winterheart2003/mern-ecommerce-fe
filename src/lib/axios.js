ace85dcb2b4781348f21ddeb2c56d1e241649d12
winterheart2003 <hellobaby2809@gmail.com>
2025-10-16 04:27:41 +0700

    u1pload

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+console.log("ev", import.meta.env.MODE);
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.mode == "development"
+    import.meta.env.MODE === "development"
       ? "/api"
       : "https://mern-ecommerce-be-production.up.railway.app/api",
   withCredentials: true,
