54e0ded748090902d994eaab2cb8372696146925
winterheart2003 <hellobaby2809@gmail.com>
2025-10-15 23:34:32 +0700

    upload1

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.mode !== "development"
+    import.meta.env.mode == "development"
       ? "/api"
       : "https://mern-ecommerce-be-production.up.railway.app/api",
   withCredentials: true,
